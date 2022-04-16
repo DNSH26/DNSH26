bd4f69bcf7c3f70773537557cba5f0740df18933
Adam <20FTT1452@student.pb.edu.bn>
2022-04-16 09:35:57 +0800

    Edited repo name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: wadt.github.io,
+  base: adamwadt.github.io,
   plugins: [vue()]
 })
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === "production" ? 
-    "wadt.github.io" : 
+    "adamwadt.github.io" : 
     "/",
 }
