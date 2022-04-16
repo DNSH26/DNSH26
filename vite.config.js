df23b452ad2a7075b6f0d9b9f8ff8010fd592ba9
Adam <20FTT1452@student.pb.edu.bn>
2022-04-16 09:42:30 +0800

    Changed repo name v2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: adamwadt.github.io,
+  // base: DNSH26.github.io,
   plugins: [vue()]
 })
